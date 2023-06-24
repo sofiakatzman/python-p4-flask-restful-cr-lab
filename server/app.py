0d6a9021e286287adedd55293c908fcfe18a6d05
Sofia Katzman <katzmansof@gmail.com>
2023-06-23 21:32:43 -0400

    finished lab

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.json.compact = True
 
 migrate = Migrate(app, db)
@@ -17,10 +17,50 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        all_plants = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            jsonify(all_plants), 
+            200
+        )
+
+        return response
+    
+    def post(self):
+        request_json = request.get_json()
+        new_plant = Plant(
+            name=request_json['name'],
+            image=request_json['image'],
+            price=request_json['price'],    
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_plant.to_dict()),
+            201
+        )
+
+        return response
+
+
+api.add_resource(Plants, '/plants',)
+
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id): 
+        found = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            jsonify(found),
+            200
+        )
+
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
---
+++ server/migrations/versions/667319b7fb8d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 667319b7fb8d
+Revises: 67f5d67aea55
+Create Date: 2023-06-23 20:15:45.158521
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '667319b7fb8d'
+down_revision = '67f5d67aea55'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
